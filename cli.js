d57c3be2aa35ed28ddbf4eab327fc1e51a7f4c11
Whyme.Lyu <5longluna@gmail.com>
2012-06-10 23:22:32 +0800

    Move HTTP stuff to lib/handler

--- cli.js
+++ cli.js
@@ -2,34 +2,13 @@
 
 var express = require("express")
   , app = express.createServer()
-  , pump = require("util").pump
   , Clipper = require("./lib/clipper/xsel")
+  , handler = require("./lib/handler").create
 
 if (!Clipper.isAvailableSync()) {
   console.error("No available clipboard bin.")
   process.exit(1)
 }
 
-app.get('/', function(req, res) {
-  var clipper = Clipper.createPasteProc()
-  res.writeHead(200, {
-    "Content-Type": "application/octet-stream"
-  })
-  clipper.on("exit", end(res))
-  pump(clipper.stdout, res, end(res))
-})
-
-app.put('/', function(req, res) {
-  var clipper = Clipper.createCopyProc()
-  res.writeHead(201)
-  clipper.on("exit", end(res))
-  pump(req, clipper.stdin, end(res))
-})
-
-function end(res) {
-  return function() {
-    res.end()
-  }
-}
-
+app.use("/", handler(Clipper))
 app.listen(2547)
