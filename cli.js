89e82f9cd634d6169cce46644633f932315c36d4
Whyme.Lyu <5longluna@gmail.com>
2012-06-10 23:31:13 +0800

    Don't have to capitalize that anymore

--- cli.js
+++ cli.js
@@ -2,13 +2,13 @@
 
 var express = require("express")
   , app = express.createServer()
-  , Clipper = require("./lib/clipper/xsel")
+  , clipper = require("./lib/clipper/xsel")
   , handler = require("./lib/handler").create
 
-if (!Clipper.isAvailableSync()) {
+if (!clipper.isAvailableSync()) {
   console.error("No available clipboard bin.")
   process.exit(1)
 }
 
-app.use("/", handler(Clipper))
+app.use("/", handler(clipper))
 app.listen(2547)
